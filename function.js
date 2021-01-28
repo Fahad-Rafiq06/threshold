ee6bfcc960953d2622cd4a43907a9fa091e0e57b
dummy <test@example.com>
2021-01-27 20:48:25 -0800

    remove alert

---
+++ function.js
@@ -0,0 +1,21 @@
+
+function shopping(a){
+    var c;
+    if (a >100){
+        c = a;
+        alert(extra +100)
+    }
+    else if (a < 100){
+        c = a;
+        alert(tax + "5")
+    }
+    return c;
+}
+var extra = "Your Shopping is upto ";
+var tax = "Your Shopping tax is ";
+
+
+var b =shopping(80);
+alert (b);
+
+
